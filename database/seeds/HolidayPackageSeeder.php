1587fbe4da2600867463a3b573eea870854c52c1
VAB1998 <blanco.vincenzo@outlook.com>
2021-11-12 15:21:29 +0100

    Seeded the DB

--- database/seeds/HolidayPackageSeeder.php
+++ database/seeds/HolidayPackageSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\HolidayPackage;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class HolidayPackageSeeder extends Seeder
@@ -9,8 +11,16 @@ class HolidayPackageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        
+        for($i = 0; $i < 5; $i++){
+            $newHolidayPackage = new HolidayPackage();
+            $newHolidayPackage->destination = $faker->words(3, true);
+            $newHolidayPackage->number_of_adults = $faker->numberBetween(1, 16);
+            $newHolidayPackage->number_of_children = $faker->numberBetween(0, 16);
+            $newHolidayPackage->departure_date = $faker->date('Y-m-d');
+            $newHolidayPackage->return_date = $faker->date('Y-m-d');
+            $newHolidayPackage->save();
+        }
     }
 }
